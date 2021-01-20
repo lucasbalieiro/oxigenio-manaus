9c0485ee31bec689b6347aecf5060b5dbdc36bf9
Lucas Balieiro <lucasbalieirom@hotmail.com>
2021-01-19 20:03:30 -0400

    ajuste de layout do map

--- src/pages.js
+++ src/pages.js
@@ -1,5 +1,4 @@
 const Database = require("./database/db");
-const saveOrphanage = require("./database/saveOrphanage");
 
 module.exports = {
   index(req, res) {
@@ -37,46 +36,11 @@ module.exports = {
         .get('locais')
         .value();
 
-      return res.render("orphanages", { locais });
+      return res.render("locals", { locais });
 
     } catch (error) {
       console.log(error);
       return res.send("Erro no banco de dados!");
     }
-  },
-
-  createOrphanage(req, res) {
-    return res.render("create-orphanage");
-  },
-
-  async saveOrphanage(req, res) {
-    const fields = req.body;
-
-    // validar se todos os campos estao preenchidos
-    if (Object.values(fields).includes("")) {
-      return res.send("Todos os campos devem ser preenchidos!");
-    }
-
-    try {
-      //salvar um orfanato
-      const db = await Database;
-      await saveOrphanage(db, {
-        lat: fields.lat,
-        lng: fields.lng,
-        name: fields.name,
-        about: fields.about,
-        whatsapp: fields.whatsapp,
-        images: fields.images.toString(),
-        instructions: fields.instructions,
-        opening_hours: fields.opening_hours,
-        open_on_weekends: fields.open_on_weekends,
-      });
-
-      // redirecionamento
-      return res.redirect("/orphanages");
-    } catch (error) {
-      console.log(error);
-      return res.send("Erro no banco de dados!");
-    }
-  },
+  }
 };
